3678e46f1222927ca0a4a3e0241529459e1172f5
ujl123 <ujl123@users.noreply.github.com>
2024-08-04 18:03:31 -0400

    source of vial firmware

---
+++ MMOAbom56/mmoabom56/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+#pragma once
+#define VIAL_KEYBOARD_UID {0x58, 0xB8, 0xF9, 0x60, 0xD9, 0xC9, 0xBC, 0xDB}
+
+
+    #define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+    #define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
